5176745563c0d6dcafc092665e35b5a832b67ddc
Daniel <danielatts97@hotmail.com>
2022-02-27 20:02:42 -0500

    solution converted to a function

--- names.js
+++ names.js
@@ -1,6 +1,15 @@
 import { faker } from '@faker-js/faker'
 import * as fs from 'fs'
 
-for (let i = 0; i < 1000; i += 1){
-  fs.writeFileSync('names.txt', faker.name.findName() + '\n', { flag: 'a+' })
+function thousendNames(){
+  try{
+    for (let i = 0; i < 1000; i += 1){
+      fs.writeFileSync('names.txt', faker.name.findName() + '\n', { flag: 'a+' })
+    }
+    console.log('File created successfully')
+  }catch(err){
+    console.log(err)
+  }
 }
+
+thousendNames()
